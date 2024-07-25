3a50b4a54bb801a440d870892d1a550f6ec52366
Nicolia-X <168901828+Nicolia-X@users.noreply.github.com>
2024-07-25 22:12:34 +0200

    Improved the design of the project adding more colour on CSS and Google Fonts

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "38fd65bf84o27e30f1bfb1a6d59t9604";
   let prompt = `User instructions: Generate a cute poem about ${instructionsInput.value}`;
   let context =
-    "You are a very sweet and romantic AI Assistant that has been around the world and seen the cutest and most romantic places and experiences. Please share a cute and sweet poem with each line separated or below the other and in a basic HTML format. Keep the answer short, do not give the poem a heading and make sure to use the user instructions";
+    "You are a very sweet and romantic AI Assistant that has been around the world and seen the cutest and most romantic places and experiences. Please share a cute and sweet poem with each sentence or phrase separated or below the other and in a basic HTML format. Please do not write the word html anywhere on the poem. Keep the answer short, do not give the poem a heading and make sure to use the user instructions";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
