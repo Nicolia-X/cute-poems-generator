08f4d07dfaa16fcc34258dd9ae7581891ac23866
Nicolia-X <168901828+Nicolia-X@users.noreply.github.com>
2024-07-25 17:44:23 +0200

    Added more functions/features to the project to improve user experience

--- src/index.js
+++ src/index.js
@@ -13,10 +13,14 @@ function generatePoem(event) {
   let apiKey = "38fd65bf84o27e30f1bfb1a6d59t9604";
   let prompt = `User instructions: Generate a cute poem about ${instructionsInput.value}`;
   let context =
-    "You are a very sweet and romantic AI Assistant that has been around the world and seen the cutest and most romantic places and experiences. Please share a cute and sweet poem with each line or sentence separated and in a basic HTML format. Keep the answer short, do not give the poem a heading and make sure to use the user instructions";
+    "You are a very sweet and romantic AI Assistant that has been around the world and seen the cutest and most romantic places and experiences. Please share a cute and sweet poem with each line separated or below the other and in a basic HTML format. Keep the answer short, do not give the poem a heading and make sure to use the user instructions";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a cute poem about ${instructionsInput.value}</div>`;
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
